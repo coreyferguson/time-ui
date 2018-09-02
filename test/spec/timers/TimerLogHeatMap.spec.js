f50bc4b1f0907aff07e4a7d319556c1c492653d1
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-09-02 08:39:09 -0700

    heat map color range unit tests

--- test/spec/timers/TimerLogHeatMap.spec.js
+++ test/spec/timers/TimerLogHeatMap.spec.js
@@ -1,5 +1,5 @@
 
-const { generateMetadata } = require('../../../src/timers/views/TimerLog/TimerLogHeatMap');
+const { generateMetadata, heatMapClassForValueGenerator } = require('../../../src/timers/views/TimerLog/TimerLogHeatMap');
 const { expect } = require('../../support/TestUtils');
 const moment = require('moment');
 const momentTz = require('moment-timezone');
@@ -209,6 +209,31 @@ describe('TimerLogHeatMap', () => {
       expect(metadata.userTimerLogs, 'userTimerLogs').to.equal(userTimerLogs);
       expect(metadata.low, 'low').to.equal(1);
       expect(metadata.high, 'high').to.equal(4);
+    });
+
+  });
+
+  describe('heatMapClassForValueGenerator', () => {
+
+    it('null userTimerLogs', () => {
+      const metadata = { userTimerLogs: undefined };
+      const fn = heatMapClassForValueGenerator(metadata);
+      expect(fn()).to.equal('color-0');
+      expect(fn({ count: 10 })).to.equal('color-0');
+    });
+
+    it('low-high: 10-60', () => {
+      const metadata = { userTimerLogs: 'not-null', low: 10, high: 60 };
+      const fn = heatMapClassForValueGenerator(metadata);
+      expect(fn()).to.equal('color-0');
+      expect(fn({ count: 10 })).to.equal('color-1');
+      expect(fn({ count: 22 })).to.equal('color-1');
+      expect(fn({ count: 23 })).to.equal('color-2');
+      expect(fn({ count: 34 })).to.equal('color-2');
+      expect(fn({ count: 35 })).to.equal('color-3');
+      expect(fn({ count: 47 })).to.equal('color-3');
+      expect(fn({ count: 48 })).to.equal('color-4');
+      expect(fn({ count: 60 })).to.equal('color-4');
     });
 
   });
